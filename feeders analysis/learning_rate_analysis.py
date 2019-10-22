d453f9feba2d4a88cd004d55a22feec54a1b177c
goni <gonina@gmail.com>
2019-10-22 11:44:25 +0300

    learning rate rfid

--- feeders analysis/learning_rate_analysis.py
+++ feeders analysis/learning_rate_analysis.py
@@ -8,10 +8,12 @@ df.index = pd.to_datetime(df.index)
 
 head = df.head()
 
+# g_df = df.groupby(['signal']).count()
 # group_df = df.groupby(pd.Grouper(freq='60Min', base=30, label='right')).first()
 # g_df = df.groupby(['feeder', pd.Grouper(key='time',base=30, freq='H')]).sum()
 group_df = df.groupby(pd.Grouper(freq='60Min', base=8, label='right'))['signal'].value_counts()
 print (head)
 print (group_df)
+print (g_df)
 # play = df['signal'].value_counts()
 # print (play)
