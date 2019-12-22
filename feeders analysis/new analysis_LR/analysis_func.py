d1c4bd9abd0fc02e00a67ea0455d87113a3ada0e
goni <gonina@gmail.com>
2019-12-22 13:50:23 +0200

    update

--- feeders analysis/new analysis_LR/analysis_func.py
+++ feeders analysis/new analysis_LR/analysis_func.py
@@ -14,17 +14,19 @@ class Data:
         self.base = 0
         self.conds = [self.df['bat2_condition'].unique()]
         self.df_min = None
+        self.df_stat = pd.DataFrame()
 
     def time_to_index(self):
         """change time column into datetime index"""
         self.df.rename(columns={'Unnamed: 0' :'time'}, inplace=True)
-        self.df.index = pd.to_datetime(self.df['time'])
+        self.df.index = pd.to_datetime(self.df['time'], dayfirst=True)
         self.df.drop(['time'], axis=1, inplace=True)
 
     def basic_stat(self): #not finished
         """ return the sum of choices that did each bat into a new df """
-        df_stat = self.df.copy().groupby('feeder').count()
-        df_stat.drop(['signal'], axis=1, inplace=True)
+        # df_stat = self.df.copy().groupby('feeder').count()
+        self.df_stat = self.df.copy().groupby('bat1_condition').count()
+        self.df_stat.drop(['signal'], axis=1, inplace=True)
         
     def pump_score(self, fill_na = True):
         """ give score 1 to each right feeder choice, score (-1) to left feeder choice,
@@ -35,10 +37,11 @@ class Data:
         self.df_score.replace({'pump_1': mapping_1, 'pump_2': mapping_2}, inplace= True)
         self.df_score['pump_1'].fillna(0, inplace= True)
         self.df_score['pump_2'].fillna(0, inplace= True)
-        if fill_na == False: # when fill_na=False only results where bat chose will be taken into account
-            self.df_score.sum_pump.replace(0, np.nan, inplace=True) 
         self.df_score['sum_pump'] = self.df_score['pump_1']+self.df_score['pump_2']
+        if fill_na == False: # when fill_na=False only results where bat chose will be taken into account
+            self.df_score.sum_pump.replace(0, np.nan, inplace=True)
         self.df_score.to_csv(f"{self.fname}_score.csv")
+
 
     # def mean_pref(self, min = '10Min'): #not finished
     #     mean_pref = self.df_score.resample(min, self.base, label='right').mean()
@@ -48,6 +51,7 @@ class Data:
         and we need to know which one activiated the feeder"""
         self.df['bat1_id'].fillna (method= 'ffill', limit= gap_limit, inplace= True)
         self.df['bat2_id'].fillna (method= 'ffill', limit= gap_limit, inplace= True)
+        self.df.to_csv(f'{fname}_fill_gaps.csv')
 
     def find_base(self):
         self.base = self.df.index[0].minute
@@ -72,11 +76,44 @@ class Data:
         self.df_min = df.iloc[idx_list_min]
         df_max = df.iloc[idx_list_max]
         self.condition_start_end = list(zip(self.df_min.index, df_max.index))
-        
+
+    def plot_all_choices(self):
+        """ """
+        pd.plotting.register_matplotlib_converters(explicit=True)
+
+        fig = plt.figure(figsize=(10,10))
+        figtemp, ax = plt.subplots(1, 1)
+        plt.style.use('seaborn')
+        choices = plt.plot_date(self.df_score.index, self.df_score['sum_pump'], linestyle=':')
+        plt.title (f'bats choices')
+        plt.ylabel('1 = right feeder, (-1) = left feeder')
+        # Set time format and the interval of ticks (every 15 minutes)
+        xformatter = md.DateFormatter('%H:%M')
+        xlocator = md.MinuteLocator(interval = 60)
+        # Set xtick labels to appear every 60 minutes
+        ax.xaxis.set_major_locator(xlocator)
+        ## Format xtick labels as HH:MM
+        plt.gcf().axes[0].xaxis.set_major_formatter(xformatter)
+        # rotate_labels
+        for label in ax.get_xticklabels():
+            label.set_rotation(40)
+            label.set_horizontalalignment('right')
+
+        cond_dict = { 'unknown': 'y', 'R reward': 'g', 'L reward': 'r'}
+        # cond_dict = { 'unknown': 'y', 'R reward': 'g', 'equal reward': 'r'}
+        # cond_dict = { self.conds[2]: 'y', self.conds[0]: 'g', self.conds[1]: 'r'}
+        for min_time,max_time in self.condition_start_end:
+            cond = self.df_min.loc[min_time]['bat2_condition']
+            plt.axvspan(min_time,max_time, alpha=0.2, color=cond_dict[cond])
+        g_patch = mpatches.Patch(color='g', label='R reward')
+        r_patch = mpatches.Patch(color='r', label='L reward')
+        plt.legend(handles=[g_patch,r_patch], loc='upper right')
+        figtemp.savefig(f'{fname}_choices_plot.png')
+
     def plot_pref (self, minutes = '10Min'): 
         """ """
-
         df_mean_pref = self.df_score.resample(minutes, base = self.base, label='right').mean()
+   
         pd.plotting.register_matplotlib_converters(explicit=True)
 
         fig = plt.figure(figsize=(10,10))
@@ -97,7 +134,8 @@ class Data:
             label.set_rotation(40)
             label.set_horizontalalignment('right')
 
-        cond_dict = { 'unknown': 'y', 'R reward': 'g', 'equal reward': 'r'}
+        cond_dict = { 'unknown': 'y', 'R reward': 'g', 'L reward': 'r'}
+        # cond_dict = { 'unknown': 'y', 'R reward': 'g', 'equal reward': 'r'}
         # cond_dict = { self.conds[2]: 'y', self.conds[0]: 'g', self.conds[1]: 'r'}
         for min_time,max_time in self.condition_start_end:
             cond = self.df_min.loc[min_time]['bat2_condition']
@@ -110,19 +148,28 @@ class Data:
 
 
 
-    
     def run(self):
         self.time_to_index()
         self.find_base()
-        self.pump_score()
+        # self.pump_score()
+        self.pump_score(fill_na=False)
         self.cond_times()
         self.plot_pref (minutes = '10Min')
+        self.plot_all_choices()
 
 
 if __name__ == "__main__":
 
-    fname = '2019-11-15.csv'
+    # fname = '2019-11-15.csv'
+    # fname = '2019-12-02_precent.csv'
+    fname = '2019-12-16_S_X.csv' #not working
+
 
     exp = Data(fname)
+    # exp.time_to_index()
+    # exp.fill_bat_id_gaps()
     exp.run()
+    # exp.basic_stat()
+    # print (exp.df_stat)
     # print (exp.df.head())
+  
