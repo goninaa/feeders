698e238586d6d1f2a4f4770d3075090a3b423f11
goni <gonina@gmail.com>
2020-05-31 20:06:43 +0300

    new

--- feeders analysis/new analysis_LR/analysis_func_v4.py
+++ feeders analysis/new analysis_LR/analysis_func_v4.py
@@ -107,10 +107,15 @@ class Data:
        
         self.df_score.to_csv(f"{self.fname}_score.csv")
 
-    def match (self): # need to fix
+    def match (self,bat): # need to fix
         """ add a column with y/n if value was as expected or not. later used for plotting"""
         # self.df_score = pd.DataFrame(self.df.copy())
         self.df_score = pd.DataFrame(self.df_filled.copy())
+        if bat==1:
+            bat_loc='bat1_loc'
+        elif bat==2:
+            bat_loc='bat2_loc'
+
         # self.df_score['match'] = np.where( 
         #                         ( (self.df_score['pump_1'] == '1') & (self.df_score['bat2_condition'] == 'L reward' ) ) 
         #                         | ( (self.df_score['pump_1'] == 'no 1') & (self.df_score['bat2_condition'] == 'R reward' ) )
@@ -127,17 +132,17 @@ class Data:
         # find all choices (only when pump gave "reward" or "no reward"):
         self.df_score['match'] = np.where( 
                                     ( ((self.df_score['pump_1'] == '1')|(self.df_score['pump_1'] == 'no 1'))&
-                                    (self.df_score['bat2_loc']==104))|
+                                    (self.df_score[bat_loc]=='Right'))|
                                     (((self.df_score['pump_2'] == '2')|(self.df_score['pump_2'] == 'no 2'))&
-                                    (self.df_score['bat2_loc']==103))
+                                    (self.df_score[bat_loc]=='Left'))
                                     , 'choice', 0) 
         # leave only reward and no rewards (drop 0)
         self.df_score = self.df_score[self.df_score.match == 'choice']
         self.df_score['match'] = np.where( 
                                     ( (self.df_score['pump_1'] == '1')&
-                                    (self.df_score['bat2_loc']==104))|
+                                    (self.df_score[bat_loc]=='Right'))|
                                     ((self.df_score['pump_2'] == '2')&
-                                    (self.df_score['bat2_loc']==103))
+                                    (self.df_score[bat_loc]=='Left'))
                                     , 'reward', 'no reward')
 
 
@@ -185,19 +190,19 @@ class Data:
         self.stay_prob_ev = self.stay_prob_ev.astype({'pump_1': 'int', 'pump_2': 'int'})
         self.stay_prob_ev['num_output'] = self.stay_prob_ev['pump_1']+self.stay_prob_ev['pump_2']
         self.stay_prob_ev['pre_choice'] = self.stay_prob_ev['num_output'].shift(1)
-        self.stay_prob_ev['choice'] = self.stay_prob_ev['bat2_loc']
+        self.stay_prob_ev['choice'] = self.stay_prob_ev[bat_loc]
         map_choice = {'Right':1,'Left':2}
-        self.stay_prob_df.replace({'choice': map_choice}, inplace= True)
+        self.stay_prob_ev.replace({'choice': map_choice}, inplace= True)
         self.stay_prob_ev.to_csv('stay_prob_ev1.csv') #for debugging
 
 
     def stay_change_to_df(self,cond, events=False): #new version
         """ create stay-change number (for specefic condition) df based on stay_prob func"""
-        # df1 = exp.stay_prob_df
         if events==True:
             df1 = self.stay_prob_ev[self.stay_prob_ev['bat2_condition']== cond].copy()
-        else:
-            df1= self.stay_prob_df[self.stay_prob_df['bat2_condition']== cond].copy()
+        elif events==False:
+            df1= self.stay_prob_df[self.stay_prob_df['bat2_condition']== cond].copy() #need to fix
+        # df1 = self.stay_prob_ev.copy()
         # print (df1)
         #create masks for pump_1
         reward_1_stay = (df1['pre_choice']==1) & (df1['choice']==1)
@@ -207,8 +212,8 @@ class Data:
         #create masks for pump_2
         reward_2_stay = (df1['pre_choice']==2) & (df1['choice']==2)
         reward_2_change = (df1['pre_choice']==2) & (df1['choice']==1)
-        unreward_2_stay = (df1['pre_choice']==-2) & (df1['choice']==2)
-        unreward_2_change = (df1['pre_choice']==-2) & (df1['choice']==1)
+        unreward_2_stay = ((df1['pre_choice']==-2)|(df1['pre_choice']==0)) & (df1['choice']==2)
+        unreward_2_change = ((df1['pre_choice']==-2)|(df1['pre_choice']==0)) & (df1['choice']==1)
         #create dict 
         reward = {'stay_1':df1[reward_1_stay].choice.count(), 'change_1':df1[reward_1_change].choice.count(),
                   'stay_2':df1[reward_2_stay].choice.count(), 'change_2':df1[reward_2_change].choice.count()}
@@ -218,22 +223,26 @@ class Data:
        
         self.stay_num_df = pd.DataFrame(proba_dict)
 
-
-    def stay_prob_to_df(self, cond= 'R reward'):
+    def stay_prob_to_df(self, cond):
         """ create stay-change probability df based on...1=right, 2=left"""
         self.proba_df = self.stay_num_df.copy().T
+        print (cond)
+        print (self.proba_df)
         if cond== 'R reward':
             self.proba_df['80% reward'] = self.proba_df['stay_1']/(self.proba_df['stay_1']+self.proba_df['change_1'])
             self.proba_df['20% reward'] = self.proba_df['stay_2']/(self.proba_df['stay_2']+self.proba_df['change_2'])
         elif cond== 'L reward':
             self.proba_df['20% reward'] = self.proba_df['stay_1']/(self.proba_df['stay_1']+self.proba_df['change_1'])
             self.proba_df['80% reward'] = self.proba_df['stay_2']/(self.proba_df['stay_2']+self.proba_df['change_2'])
-        self.proba_df.drop(['stay_1','change_1','stay_2','change_2'], axis=1, inplace=True)
+            self.proba_df.drop(['stay_1','change_1','stay_2','change_2'], axis=1, inplace=True)
+        else:
+            raise ValueError('cond must be "R reward" or "L reward"')
+        print (self.proba_df)
         self.proba_df=self.proba_df[['80% reward','20% reward']] #keeps the same colmuns order
   
 
-    def plot_prob(self,cond='R reward'):
-        colors = {'r','b'}
+    def plot_prob(self,cond):
+        colors = {'b','r'}
         ax = self.proba_df.plot.bar(color=colors)
         ax.set_title('Stay Probability')
         L=plt.legend()
@@ -241,11 +250,14 @@ class Data:
         L.get_texts()[0].set_text('80% reward')
         L.get_texts()[1].set_text('20% reward')
         ax.set_ylabel('Stay probability')
+        fig = ax.get_figure()
+        fig.savefig(f'{fname}_stay_prob.png')
+        fig.savefig(f'{fname}_stay_prob.svg')
      
-    def run_prob_ev(self,cond):
+    def run_prob_ev(self,cond='L reward',bat=2):
         """stay-probability plot by events """
-        self.stay_prob_events()
-        self.stay_change_to_df(cond, events=True)
+        self.stay_prob_events(bat=bat)
+        self.stay_change_to_df(cond=cond, events=True)
         self.stay_prob_to_df(cond)
         self.plot_prob(cond)
 
@@ -253,7 +265,7 @@ class Data:
     def run_prob(self,cond):
         """stay-probability plot by choices """
         self.stay_prob()
-        self.stay_change_to_df(cond)
+        self.stay_change_to_df(cond, events=False)
         self.stay_prob_to_df(cond)
         self.plot_prob(cond)
 
@@ -427,9 +439,22 @@ class Data:
         figtemp.savefig(f'{fname}_choices_plot_{name}.svg')
 
 
-    def plot_pref (self, minutes = '10Min', name= 'choices_only'): 
+    def plot_pref (self, minutes = '10Min', name= 'choices_only', events = False): #changed
         """ """
-        df_mean_pref = self.df_score.resample(minutes, base = self.base, label='right').mean()
+        if events == False: 
+            df_mean_pref = self.df_score.resample(minutes, base = self.base, label='right').mean()
+        elif events == True:
+            df_mean_pref['sum_pump']= df_mean_pref['choice']
+            mapping1 = {1: -1}
+            mapping2 = {2: 1}
+            df_mean_pref = df_mean_pref.astype({'sum_pump': 'object'})
+            df_mean_pref.replace({'sum_pump': mapping1}, inplace= True)
+            df_mean_pref.replace({'sum_pump': mapping2}, inplace= True)
+            df_mean_pref.sum_pump.fillna(0, inplace= True)
+            df_mean_pref = self.stay_prob_ev.resample(minutes, base = self.base, label='right').mean()
+            print (df_mean_pref['sum_pump'])
+            # df_mean_pref = df_mean_pref.astype({'choice': 'int'})  
+           
    
         pd.plotting.register_matplotlib_converters(explicit=True)
 
@@ -475,7 +500,7 @@ class Data:
         # map_2 = {102: 'left'}
         self.df_filled.replace({'bat1_loc': map_1, 'bat2_loc': map_1}, inplace=True)
 
-    def plot_bat_movement(self, bat=2):
+    def plot_bat_movement(self, bat):
         """ plots bat movement and reward for all events (not choices)
             input: self.df_filled, self.df_min_ev,
             output: movement and rewards plot """
@@ -510,6 +535,7 @@ class Data:
             sns_fig.set_ylabel('Feeder')
             sns_fig.set_title(f'Bat {bat} movement and rewards')
             # plt.plot(self.df_filled[bat_loc]) # works
+            figtemp.tight_layout()
             plt.show()
             figtemp.savefig(f'{fname}_movement_plot.png')
             figtemp.savefig(f'{fname}_movement_plot.svg')
@@ -518,7 +544,7 @@ class Data:
             print (e, 'bat can be 1 or 2')
 
 
-    def run_landings_chunks (self): #in progress
+    def run_landings_chunks (self,bat, cond):
         """count landing chunks as events on each feeder"""
         self.time_to_index()
         self.fill_bat_id_gaps()
@@ -526,13 +552,15 @@ class Data:
         self.fill_cond_gaps()
         self.fill_pumps_gaps()
         self.map_feeders()
-        self.start_event()
-        self.mark_reward()
-        self.plot_bat_movement()
-        self.run_prob_ev('L_reward')
+        self.start_event(bat)
+        self.mark_reward(bat)
+        self.plot_bat_movement(bat=bat)
+        self.run_prob_ev(cond=cond,bat=bat)
+        self.plot_pref(minutes='10Min',name='Mean choices', events=True)
+        # self.run_prob_ev(cond='R reward')
     #    print (self.df_filled['bat1_loc'])
 
-    def start_event (self, bat =2): #still in progress
+    def start_event (self, bat): #still in progress
         """ return new df of the feeder first landed in each event start
             input: self.df_filled, 
             output: self.df_min_ev, self.event_start_end"""
@@ -545,7 +573,6 @@ class Data:
             df_no_idx = df.reset_index()
             df_feeder = pd.concat([df_no_idx[bat_loc], df_no_idx[bat_loc].shift()], axis=1)
             df_feeder.columns = ['loc1', 'loc2']
-            print (df_feeder)
             # df_feeder = df_feeder.fillna(method='ffill')
             idx = df_feeder[df_feeder['loc1'] != df_feeder['loc2']].index
             idx_list = list(idx)
@@ -561,7 +588,7 @@ class Data:
         except NameError as e:
             print (e, 'bat can be 1 or 2')
 
-    def mark_reward (self,bat =2): #not working yet
+    def mark_reward (self,bat): #not working yet
         """ mark all often and rare rewards, bat can be 1 or 2
             input: self.df_min_ev, output: self.df_min_ev"""
         # self.df_min_ev['mark'] = np.where( 
@@ -586,10 +613,10 @@ class Data:
         
 
 
-    def run_fill_na(self):
+    def run_fill_na(self,bat):
         self.time_to_index()
         self.find_base()
-        self.match()
+        self.match(bat)
         self.pump_score()
         # self.pump_score(fill_na=False)
         self.cond_times()
@@ -599,20 +626,21 @@ class Data:
         self.basic_stat()
         # print (self.df_stat)
 
-    def run(self):
+    def run(self,bat):
         self.time_to_index()
         self.find_base()
         self.fill_bat_id_gaps()
         self.fill_bat_loc_gaps()
         self.fill_cond_gaps()
-        self.match() 
+        self.map_feeders()
+        self.match(bat) 
         # self.pump_score()
         self.pump_score(fill_na=False) 
         # self.map_feeders()
         self.cond_times()
-        # self.plot_pref (minutes = '10Min', name = 'choices_only')
-        # self.plot_pref (minutes = '60Min', name = 'choices_only')
-        self.plot_all_choices_match('choices_only') # need to fix?
+        self.plot_pref (minutes = '10Min', name = 'choices_only')
+        self.plot_pref (minutes = '60Min', name = 'choices_only')
+        # self.plot_all_choices_match('choices_only') # need to fix?
         # self.fill_bat_id_gaps()
         self.basic_stat()
         # print (self.df_stat)
@@ -622,7 +650,9 @@ if __name__ == "__main__":
 
     # fname = '2020-03-09-08_A_Gimel_fast.csv'
     # fname = '/Users/gonina/Library/Mobile Documents/com~apple~CloudDocs/lab/python_codes/feeders/feeders analysis/2020-04-02-09_B_Eight_slow_cut_midnight.csv'
-    fname = '/Users/gonina/Library/Mobile Documents/com~apple~CloudDocs/lab/python_codes/feeders/feeders analysis/2020-03-24-05_B_Lamed_slow.csv'
+    # fname = '/Users/gonina/Library/Mobile Documents/com~apple~CloudDocs/lab/python_codes/feeders/feeders analysis/2020-03-24-05_B_Lamed_slow.csv'
+    # fname = '/Users/gonina/Library/Mobile Documents/com~apple~CloudDocs/lab/python_codes/feeders/feeders analysis/2020-04-24-08_A_Arrow_slow.csv'
+    fname = '/Users/gonina/Library/Mobile Documents/com~apple~CloudDocs/lab/python_codes/feeders/feeders analysis/2020-03-30-06_A_Tzadi_slow.csv'
     # fname = '2020-01-20_A_train_zurik_lamed.csv'
     # fname = '2019-12-06_F_percent.csv'
     # fname = '2019-12-16_S_X.csv' #not working
@@ -635,13 +665,16 @@ if __name__ == "__main__":
     exp = Data(fname)
     # exp.run_fill_na()
     # exp = Data(fname)
-    # exp.run() # works (choices only)
-    exp.run_landings_chunks() #works
+
+    # exp.run(bat=1) # works (choices only)
+    # exp.run_prob('L reward') #works (choices only)
+    exp.run_landings_chunks(bat=1, cond= 'L reward') #works
+
     # print (exp.df_min_ev['bat2_loc'].head(10))
     # print (exp.df_min_ev.head(10))
     # print (exp.df_filled['bat2_loc'].unique)
     # print (exp.df_filled[exp.df_filled['bat2_loc']=='Left'])
-    # exp.run_prob('L reward') #works
+
 
     # exp.time_to_index()
     # exp.find_base()
